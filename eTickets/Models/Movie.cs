f356c0fba7752450c2afb3bdc106a5db50f7208b
VARGHESE, Vibin <Vibin.Varghese@ge.com>
2021-12-08 14:59:20 +0530

    Added AppDbContext

---
+++ eTickets/Models/Movie.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using eTickets.Data;
+using eTickets.Data.Enums;
+
+namespace eTickets.Models
+{
+    public class Movie
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public double Price { get; set; }
+
+        public string ImageURL { get; set; }
+
+        public DateTime StartTime { get; set; }
+
+        public DateTime EndTime { get; set; }
+
+        public MovieCategory MovieCategory { get; set; }
+    }
+}
--- eTickets/Startup.cs
+++ eTickets/Startup.cs
@@ -1,10 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using eTickets.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -23,6 +19,9 @@ namespace eTickets
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //DBContext Configuration
+            services.AddDbContext<AppDbContext>();
+
             services.AddControllersWithViews();
         }
 
