f13532d97c061ee7bdc483c82a3b793deeb285dd
VARGHESE, Vibin <Vibin.Varghese@ge.com>
2021-12-13 15:15:31 +0530

    Added some Razor pages

--- eTickets/Controllers/CinemasController.cs
+++ eTickets/Controllers/CinemasController.cs
@@ -1,5 +1,7 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using eTickets.Data;
+using eTickets.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -16,8 +18,46 @@ namespace eTickets.Controllers
 
         public async Task<IActionResult> Index()
         {
-            var allCinemas = _context.Cinemas.ToListAsync();
-            return View();
+            //var allCinemas = _context.Cinemas.ToListAsync();
+            var cinemas = GetCinemas();
+            return View(cinemas);
+        }
+
+        private IList<Cinema> GetCinemas()
+        {
+            return new List<Cinema>()
+            {
+                new Cinema()
+                {
+                    Name = "Cinema 1",
+                    Logo = "http://dotnethow.net/images/cinemas/cinema-1.jpeg",
+                    Description = "This is the description of the first cinema"
+                },
+                new Cinema()
+                {
+                    Name = "Cinema 2",
+                    Logo = "http://dotnethow.net/images/cinemas/cinema-2.jpeg",
+                    Description = "This is the description of the first cinema"
+                },
+                new Cinema()
+                {
+                    Name = "Cinema 3",
+                    Logo = "http://dotnethow.net/images/cinemas/cinema-3.jpeg",
+                    Description = "This is the description of the first cinema"
+                },
+                new Cinema()
+                {
+                    Name = "Cinema 4",
+                    Logo = "http://dotnethow.net/images/cinemas/cinema-4.jpeg",
+                    Description = "This is the description of the first cinema"
+                },
+                new Cinema()
+                {
+                    Name = "Cinema 5",
+                    Logo = "http://dotnethow.net/images/cinemas/cinema-5.jpeg",
+                    Description = "This is the description of the first cinema"
+                },
+            };
         }
     }
 }
--- eTickets/Controllers/ProducersController.cs
+++ eTickets/Controllers/ProducersController.cs
@@ -1,5 +1,7 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using eTickets.Data;
+using eTickets.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -16,8 +18,47 @@ namespace eTickets.Controllers
 
         public async Task<IActionResult> Index()
         {
-            var data = await _context.Producers.ToListAsync();
-            return View();
+            //var data = await _context.Producers.ToListAsync();
+            var data = GetProducers();
+            return View(data);
+        }
+
+        private List<Producer> GetProducers()
+        {
+            return new List<Producer>()
+            {
+                new Producer()
+                {
+                    FullName = "Producer 1",
+                    Bio = "This is the Bio of the first actor",
+                    ProfilePictureURL = "http://dotnethow.net/images/producers/producer-1.jpeg"
+
+                },
+                new Producer()
+                {
+                    FullName = "Producer 2",
+                    Bio = "This is the Bio of the second actor",
+                    ProfilePictureURL = "http://dotnethow.net/images/producers/producer-2.jpeg"
+                },
+                new Producer()
+                {
+                    FullName = "Producer 3",
+                    Bio = "This is the Bio of the second actor",
+                    ProfilePictureURL = "http://dotnethow.net/images/producers/producer-3.jpeg"
+                },
+                new Producer()
+                {
+                    FullName = "Producer 4",
+                    Bio = "This is the Bio of the second actor",
+                    ProfilePictureURL = "http://dotnethow.net/images/producers/producer-4.jpeg"
+                },
+                new Producer()
+                {
+                    FullName = "Producer 5",
+                    Bio = "This is the Bio of the second actor",
+                    ProfilePictureURL = "http://dotnethow.net/images/producers/producer-5.jpeg"
+                }
+            };
         }
     }
 }
